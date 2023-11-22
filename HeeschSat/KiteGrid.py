91ae0c9c8ee28fd09ba676c6d045e58adce5f3a3
Lucas Ortengren <lucasortengren@gmail.com>
2023-11-21 19:06:12 -0600

    added complete get_transformations() method

--- HeeschSat/KiteGrid.py
+++ HeeschSat/KiteGrid.py
@@ -1,8 +1,9 @@
-import numpy
 import numpy as np
 
 from HeeschSat.Grid import Grid
 from math import sqrt
+
+from collections import deque
 
 
 class KiteGrid(Grid):
@@ -52,7 +53,7 @@ class KiteGrid(Grid):
                 )
 
     @staticmethod
-    def _rotate_once(tile: list):
+    def _rotate(tile: list):
         rot_mat = np.array([[0, -1],
                             [1, 1]])
         # transformation will be an array of each original kite rotated once.
@@ -68,4 +69,44 @@ class KiteGrid(Grid):
 
     @staticmethod
     def _reflect(tile: list):
-        pass
+        ref_mat = np.array([[-1, 0],
+                            [0, 1]])
+        # transformation will be a list of each original kite reflected.
+        transformation = []
+        for kite in tile:
+            # multiply first two coords by reflection matrix
+            new_kite = np.dot(ref_mat, kite[:2])
+            # handles 3rd coordinate
+            match kite[2]:
+                case 0:
+                    new_kite.append(2)
+                case 1:
+                    new_kite.append(1)
+                case 2:
+                    new_kite.append(0)
+                case 3:
+                    new_kite.append(5)
+                case 4:
+                    new_kite.append(4)
+                case 5:
+                    new_kite.append(3)
+            transformation.append(new_kite)
+
+        return transformation
+
+    @staticmethod
+    def get_transformations(tile: list):
+        # will contain each possible transformation of given tile
+        stack = deque()
+        # add original tile and reflected tile to stack
+        stack.append(KiteGrid._reflect(tile))
+        stack.append(tile)
+        while KiteGrid._rotate(stack[-1]) != tile:
+            # find reflected & non-reflected rotation of most recently added tile
+            ref_rot_tile = KiteGrid._reflect(KiteGrid._rotate(stack[-1]))
+            rot_tile = KiteGrid._rotate(stack[-1])
+            # push new tiles to stack
+            stack.append(ref_rot_tile)
+            stack.append(rot_tile)
+
+        return list(stack)
