8662fac3ec834e81ef6b8c9f8f9e3fbc4ebf850d
Gugu0099 <julati@wisc.edu>
2023-12-03 23:22:03 -0600

    reading files + transforming working

--- HeeschSat/PlotKite.py
+++ HeeschSat/PlotKite.py
@@ -1,4 +1,8 @@
 import os, glob
+import numpy as np
+import ast
+import re
+from math import floor
 
 kite_points = []
 path = "../JosephMyers/"
@@ -6,23 +10,30 @@ for filename in glob.glob(os.path.join(path, "*.txt")):
     with open(os.path.join(os.getcwd(), filename), "r") as f:  # open in readonly mode
         # do your stuff
         # Using readlines()
+        startValue = 0
+        endValue = 2
         Lines = f.readlines()
-
-        count = 0
+        lineCount = 0
         # Strips the newline character
         print("New File")
         for line in Lines:
-            count += 1
-            t = line.strip()
-            # Data points
-            # kite_points.append(t.spilt(") "))
-            # print(kite_points)
-            print("Line{}: {}".format(count, line.strip()))
+            if lineCount not in range(startValue, endValue):
+                lineCount += 1
+                continue
+            dataList = [
+                tuple(map(int, match.group(1).split(",")))
+                for match in re.finditer(r"\((.*?)\)", line)
+            ]
+            for a, b in dataList:
+                x = floor(a / 3)
+                y = floor(b / 2)
+                k = floor((b % 2) * 3 + (a % 3))
+                print(x, y, k)
+            lineCount += 1
 
 
 """
-    
-    The coordinate model is related to that for polyhexes.  Each hexagon
+The coordinate model is related to that for polyhexes.  Each hexagon
 in $(6^3)$, oriented as in the coordinate model for polyhexes, is
 divided into six kites; consider those to be numbered $0$ through~$5$
 counterclockwise, with numbers $0$ through~$2$ being in the bottom
@@ -30,13 +41,6 @@ half of the hexagon.  Then, in hexagon $(x, y)$, kite number~$k$ is
 given polykite coordinates $(3x + (k mod 3), 2y + floor(k/3))$.
     
 """
-a = 0
-b = 1
-# p = [a, b]
-
-x = a / 3
-y = b / 2
-k = (b % 2) * 3 + (a % 3)
 
 
 # TODO: transform (a,b) to (x,y,k) using above equations
